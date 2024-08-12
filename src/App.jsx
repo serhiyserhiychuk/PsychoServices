47407a77de6c93c574bd37de7e222a528066da26
serhiyserhiychuk <serhiy.serhiychuk07@gmail.com>
2024-08-12 13:19:58 -0400

    add not found page and .env

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const PsychologistsPage = lazy(() =>
   import("./pages/PsychologistsPage/PsychologistsPage")
 );
 const FavoritesPage = lazy(() => import("./pages/FavoritesPage/FavoritesPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 import Loader from "./components/Loader/Loader";
 import Navigation from "./components/Navigation/Navigation";
 
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/psychologists" element={<PsychologistsPage />} />
           <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </>
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,14 +3,14 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBh2sV-xoKDVfLKcFBr1703AvG_pqp9K9U",
-  authDomain: "psychoservices-2425f.firebaseapp.com",
-  projectId: "psychoservices-2425f",
-  storageBucket: "psychoservices-2425f.appspot.com",
-  messagingSenderId: "456372329674",
-  appId: "1:456372329674:web:3afcdba80e031d1d192833",
-  measurementId: "G-938XFZBCLS",
-  databaseURL: "https://psychoservices-2425f-default-rtdb.firebaseio.com",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
